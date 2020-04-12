c41c4ea82b1583051425f77e49767177f5a62913
jduncan66 <jerry@opespacific.io>
2020-04-12 13:29:27 -0700

    newest JSON changes - redirect URL for the logo

--- public/nodeJSON.js
+++ public/nodeJSON.js
@@ -1,65 +1,66 @@
 const NodeJSON = {
-    "schema": "https://opes-community.github.io/data-schemas/self-desc/jsonschema/1-0-0#",
-    "community_node_account_name": "adventures",
-    "org": {
-        "display_name": "OPES Adventures",
-        "website": "https://www.opesadventures.com",
-        "pledge_to_opes_ecosystem": "Blockchain for the Venturous Soul",
-        "email": "opesadventures@gmail.com",
-        "branding":{
-            "sphere_256": "https://www.opesadventures.com/branding/logo256",
-            "sphere_1024": "",
-            "sphere_svg": ""
-        },
-        "location": {
-            "name": "Junction City, Oregon",
-            "country": "United States of America",
-            "latitude":  44.219230,
-            "longitude": -123.204690
-        },
-        "social": {
-            "linkedin": "",
-            "instagram": "",
-            "snapchat": "",
-            "steemit": "",
-            "twitter": "",
-            "youtube": "",
-            "facebook": "",
-            "github": "",
-            "reddit": "",
-            "keybase": "",
-            "telegram": "",
-            "wechat": ""
-        }
+  "schema": "https://opes-community.github.io/data-schemas/self-desc/jsonschema/1-0-0#",
+  "community_node_account_name": "adventures",
+  "org": {
+    "display_name": "OPES Adventures",
+    "website": "https://www.opesadventures.com",
+    "summary": "Blockchain for the Venturous Soul",
+    "pledge_to_opes_ecosystem": "Blockchain for the Venturous Soul",
+    "email": "opesadventures@protonmail.com",
+    "branding":{
+      "sphere_256": "https://opes-adventure-api.herokuapp.com/branding/logo256",
+      "sphere_1024": "",
+      "sphere_svg": ""
     },
-    "nodes": [
-        {
-            "location": {
-                "name": "San Francisco",
-                "country": "USA",
-                "latitude":  37.774929,
-                "longitude": -122.419418
-            },
-            "node_type": "infra",
-            "p2p_endpoint": "",
-            "bnet_endpoint": "",
-            "api_endpoint": "",
-            "ssl_endpoint": ""
-        },
-        {
-            "location": {
-                "name": "",
-                "country": "",
-                "latitude": 0,
-                "longitude": 0
-            },
-            "node_type": "seed",
-            "p2p_endpoint": "",
-            "bnet_endpoint": "",
-            "api_endpoint": "",
-            "ssl_endpoint": ""
-        }
-    ]
+    "location": {
+      "name": "Junction City, Oregon",
+      "country": "United States of America",
+      "latitude":  44.219230,
+      "longitude": -123.204690
+    },
+    "social": {
+      "linkedin": "",
+      "instagram": "",
+      "snapchat": "",
+      "steemit": "",
+      "twitter": "",
+      "youtube": "",
+      "facebook": "",
+      "github": "",
+      "reddit": "",
+      "keybase": "",
+      "telegram": "",
+      "wechat": ""
+    }
+  },
+  "nodes": [
+    {
+      "location": {
+        "name": "San Francisco",
+        "country": "USA",
+        "latitude":  37.774929,
+        "longitude": -122.419418
+      },
+      "node_type": "infra",
+      "p2p_endpoint": "",
+      "bnet_endpoint": "",
+      "api_endpoint": "",
+      "ssl_endpoint": ""
+    },
+    {
+      "location": {
+        "name": "",
+        "country": "",
+        "latitude": 0,
+        "longitude": 0
+      },
+      "node_type": "seed",
+      "p2p_endpoint": "",
+      "bnet_endpoint": "",
+      "api_endpoint": "",
+      "ssl_endpoint": ""
+    }
+  ]
 };
 
 module.exports = NodeJSON;
