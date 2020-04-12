b6efb32709b8ac4d903e12c34c67a2efa4846e4d
Blake <bryantb2@my.lanecc.edu>
2020-04-12 12:44:41 -0700

    added JSON file for Jerry :p

--- routes/infoRoute.js
+++ routes/infoRoute.js
@@ -1,73 +1,10 @@
+const NodeJSON = require('../public/nodeJSON');
 const express = require('express');
 const router = express.Router();
 
 router.get('/json', async (req,res) => {
     console.log('route json has fired')
-    const communityNode = {
-  "schema": "https://opes-community.github.io/data-schemas/self-desc/jsonschema/1-0-0#",
-  "community_node_account_name": "adventures",
-  "org": {
-    "display_name": "OPES Adventures",
-    "website": "https://www.opesadventures.com",
-    "pledge_to_opes_ecosystem": "Blockchain for the Venturous Soul",
-    "email": "opesadventures@gmail.com",
-    "branding":{
-      "sphere_256": "https://www.opesadventures.com/branding/logo256",
-      "sphere_1024": "",
-      "sphere_svg": ""
-    },
-    "location": {
-      "name": "Junction City, Oregon",
-      "country": "United States of America",
-      "latitude":  44.219230,
-      "longitude": -123.204690
-    },
-    "social": {
-      "linkedin": "",
-      "instagram": "",
-      "snapchat": "",
-      "steemit": "",
-      "twitter": "",
-      "youtube": "",
-      "facebook": "",
-      "github": "",
-      "reddit": "",
-      "keybase": "",
-      "telegram": "",
-      "wechat": ""
-    }
-  },
-  "nodes": [
-    {
-      "location": {
-        "name": "San Francisco",
-        "country": "USA",
-        "latitude":  37.774929,
-        "longitude": -122.419418
-      },
-      "node_type": "infra",
-      "p2p_endpoint": "",
-      "bnet_endpoint": "",
-      "api_endpoint": "",
-      "ssl_endpoint": ""
-    },
-    {
-      "location": {
-        "name": "",
-        "country": "",
-        "latitude": 0,
-        "longitude": 0
-      },
-      "node_type": "seed",
-      "p2p_endpoint": "",
-      "bnet_endpoint": "",
-      "api_endpoint": "",
-      "ssl_endpoint": ""
-    }
-  ]
-};
-    
-    res.send(communityNode);
+    res.send(NodeJSON);
 });
 
 module.exports = router;
